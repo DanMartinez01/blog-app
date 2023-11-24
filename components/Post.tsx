632bba16f21b70d9dc98dd7acd8683ca0f69f563
Daniela <martinezdl_@hotmail.com>
2023-11-24 16:57:56 -0300

    create Post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -13,12 +13,14 @@ export type PostProps = {
   published: boolean;
 };
 
-const Post: React.FC<{ post: PostProps }> = ({ post }) => {
+const Post: React.FC<{ post: PostProps}> = ({ post }) => {
   const authorName = post.author ? post.author.name : "Unknown author";
+
   return (
     <div onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}>
       <h2>{post.title}</h2>
       <small>By {authorName}</small>
+      
       <ReactMarkdown children={post.content} />
       <style jsx>{`
         div {
